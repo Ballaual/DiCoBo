028396563687bfea40c68640d3d845feb8c635dc
Alexander Ballauf <ballaual@gmail.com>
2023-03-31 12:09:06 +0200

    fixed missing emojis for the info command

--- commands/Bot/info.js
+++ commands/Bot/info.js
@@ -6,52 +6,52 @@ const botver = require("../../version.json").version
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("info")
-        .setDescription("Send detailed info about the client"),
+        .setDescription("Send detailed info about the bot"),
     async execute (interaction) {
         const client = interaction.client
         const embed = new EmbedBuilder()
             .setThumbnail(client.user.displayAvatarURL())
-            .setTitle("Bot Stats")
+            .setTitle("Bot Information")
             .setColor("#7200FF")
             .addFields(
                 {
-                    name: "<:author:879515584767873024> Author",
-                    value: `${client.users.cache.get(process.env.Admin)?.tag}`,
+                    name: "<:author:1091296385388773437> Author",
+                    value: `${client.users.cache.get(process.env.adminID)?.tag}`,
                     inline: true
                 },
                 {
-                    name: "<:server:879374547864936448> Servers",
-                    value: `Serving ${client.guilds.cache.size} servers.`,
+                    name: "<:discord:1091295194818818098> Servers",
+                    value: `Serving ${client.guilds.cache.size} servers`,
                     inline: true
                 },
                 {
-                    name: "<:channels:879515584407162982> Channels",
-                    value: `Serving ${client.channels.cache.size} channels.`,
+                    name: "<:channels:1091302046201810984> Channels",
+                    value: `Serving ${client.channels.cache.size} channels`,
                     inline: true
                 },
                 {
-                    name: "<:user:879371469048664115> Users",
-                    value: `Serving ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users.`,
+                    name: "<:user:1091295188762243114> Users",
+                    value: `Serving ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users`,
                     inline: true
                 },
                 {
-                    name: "<:join:879517590454689852> Join Date",
+                    name: "<:join:1091295564483792926> Join Date",
                     value: client.user.createdAt.toLocaleDateString("en-us"),
                     inline: true
                 },
                 {
-                    name: "<:djs:879371469094805564> Discord.js Version",
-                    value: `${version}`,
+                    name: "<:djs:1091295192956547072> Discord.js Version",
+                    value: `\`${version}\``,
                     inline: true
                 },
                 {
-                    name: "<:node:879371469015097374> Node.js Version",
-                    value: `${process.version}`,
+                    name: "<:node:1091294141641674852> Node.js Version",
+                    value: `\`${process.version}\``,
                     inline: true
                 },
                 {
-                    name: "<:github:882479353185833020> Bot Version",
-                    value: `v${botver}`,
+                    name: "<:github:1091295909972803616> Bot Version",
+                    value: `\`v${botver}\``,
                     inline: true
                 },
                 {
@@ -65,12 +65,12 @@ module.exports = {
                     inline: true
                 },
                 {
-                    name: "<:memory:879371468876701786> Memory",
-                    value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}mb`,
+                    name: "<:memory:1091301379928236074> Memory",
+                    value: `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} mb\``,
                     inline: true
                 },
                 {
-                    name: "<:cpu:879371469052846110> CPU",
+                    name: "<:cpu:1091295423915892737> CPU",
                     value: `\`\`\`md\n${os.cpus().map((i) => `${i.model}`)[0]}\`\`\``,
                     inline: true
                 }
