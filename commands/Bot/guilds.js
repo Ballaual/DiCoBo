639d64693bb5807afc50ee139f7a6dab404a803a
Alexander Ballauf <ballaual@gmail.com>
2023-03-31 12:36:52 +0200

    remove owner check when using guilds command

--- commands/Bot/guilds.js
+++ commands/Bot/guilds.js
@@ -1,16 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-
 const { EmbedBuilder } = require("discord.js")
-require("dotenv").config()
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("guilds")
         .setDescription("Displays the top 50 guilds the bot is in"),
     async execute (interaction) {
-        if (interaction.member.id !== process.env.adminID) {
-            return interaction.reply({ content: "Bot owner only!" })
-        }
         const guilds = interaction.client.guilds.cache
             .sort((a, b) => b.memberCount - a.memberCount)
             .first(50)
@@ -18,7 +13,7 @@ module.exports = {
             return `${index + 1}) ${guild.name} | ${guild.memberCount} members | ID:${guild.id} `
         }).join("\n")
         const embed = new EmbedBuilder()
-            .setTitle(`${interaction.client.user.username}'s top Guilds:`)
+            .setTitle(`${interaction.client.user.username}'s Top 50 servers:`)
             .setThumbnail(interaction.client.user.displayAvatarURL())
             .setColor("#FFFF00")
             .setDescription(description)
