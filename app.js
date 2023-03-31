365ef76db55ed138d6f626703bb8a1fa798e2eb7
Alexander Ballauf <ballaual@gmail.com>
2023-03-31 12:11:00 +0200

    corrected false command information for updating

--- app.js
+++ app.js
@@ -293,7 +293,7 @@ import("node-fetch")
                     console.log("\x1b[32m%s\x1b[0m", "===============================Update Available===================================")
                     console.log("Ver:", data.version)
                     console.log("\x1b[36m%s\x1b[0m", "Check commit : https://github.com/ballaual/DiCoBo/commits/master")
-                    console.log("\x1b[31m%s\x1b[0m", "Use `npm run updatebot` to update")
+                    console.log("\x1b[31m%s\x1b[0m", "Use `npm run update` to update")
                     console.log("\x1b[32m%s\x1b[0m", "==================================================================================")
                 } else {
                     console.log("\x1b[32m%s\x1b[0m", "No Update Available")
