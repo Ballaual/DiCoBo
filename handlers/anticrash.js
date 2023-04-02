6c6bcdc58e4b71e16a79446eb0cb0f555d5f6a41
Alexander Ballauf <ballaual@gmail.com>
2023-04-02 02:57:12 +0200

    merged into errors.js

--- handlers/anticrash.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client) => {
-    process.on("unhandledRejection", (reason, p) => {
-        console.log(reason, p)
-    })
-    process.on("uncaughtException", (err, origin) => {
-        console.log(err, origin)
-    })
-}
