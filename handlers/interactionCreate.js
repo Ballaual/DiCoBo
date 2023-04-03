d892376de235c88d803c53b97869f015ed40ab32
Alexander Ballauf <ballaual@gmail.com>
2023-04-03 16:58:44 +0200

    delete unused stuff

--- handlers/interactionCreate.js
+++ handlers/interactionCreate.js
@@ -37,36 +37,3 @@ module.exports = (client) => {
         executeInteraction(interaction, client.commands)
     })
 }
-
-// TO-DO - Merge these stuff
-
-// const humanizeDuration = require("humanize-duration")
-// const Timeout = new Set()
-
-/* client.on("interactionCreate", async (interaction) => {
-    if (interaction.isCommand() || interaction.isContextMenu()) {
-        if (!client.slash.has(interaction.commandName)) return
-        if (!interaction.guild) return
-        const command = client.slash.get(interaction.commandName)
-        try {
-            if (command.timeout) {
-                if (Timeout.has(`${interaction.user.id}${command.name}`)) {
-                    return interaction.reply({ content: `You need to wait **${humanizeDuration(command.timeout, { round: true })}** to use command again`, ephemeral: true })
-                }
-            }
-            if (command.permissions) {
-                if (!interaction.member.permissions.has(command.permissions)) {
-                    return interaction.reply({ content: `:x: You need \`${command.permissions}\` to use this command`, ephemeral: true })
-                }
-            }
-            command.run(interaction, client)
-            Timeout.add(`${interaction.user.id}${command.name}`)
-            setTimeout(() => {
-                Timeout.delete(`${interaction.user.id}${command.name}`)
-            }, command.timeout)
-        } catch (error) {
-            console.error(error)
-            await interaction.reply({ content: ":x: There was an error while executing this command!", ephemeral: true })
-        }
-    }
-}); */
