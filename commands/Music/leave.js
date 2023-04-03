e5f91daf51736d00a80f654469a1718a0651b849
Alexander Ballauf <ballaual@gmail.com>
2023-04-03 14:41:50 +0200

    fixed permissions to use leave command

--- commands/Music/leave.js
+++ commands/Music/leave.js
@@ -1,25 +1,46 @@
-const { SlashCommandBuilder } = require("@discordjs/builders")
-const { joinVoiceChannel } = require("@discordjs/voice")
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("leave")
-        .setDescription("The bot leaves the current voice channel"),
+  data: new SlashCommandBuilder()
+    .setName("leave")
+    .setDescription("The bot leaves the current voice channel"),
 
-    async execute (interaction) {
-        const voiceChannel = interaction.member.voice.channel
+  async execute(interaction) {
+    const voiceChannel = interaction.member.voice.channel;
+    const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
 
-        if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
-        }
+    if (!voiceChannel && !botMember.voice?.channelId) {
+      return interaction.reply({
+        content: "The bot is currently not in a voice channel.",
+        ephemeral: true,
+      });
+    } else if (voiceChannel && !botMember.voice?.channelId) {
+      return interaction.reply({
+        content: "I'm not currently in a voice channel!",
+        ephemeral: true,
+      });
+    } else if (!voiceChannel) {
+        return interaction.reply({
+          content: "You need to be in the same voice channel as the bot to use this command!",
+          ephemeral: true,
+        });
+      }
 
-        const connection = joinVoiceChannel({
-            channelId: voiceChannel.id,
-            guildId: interaction.guildId,
-            adapterCreator: interaction.guild.voiceAdapterCreator
-        })
+    const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: interaction.guildId,
+        adapterCreator: interaction.guild.voiceAdapterCreator,
+    });
 
-        connection.destroy()
-        await interaction.reply("***Successfully left the voice channel***")
+    if (!connection) {
+      return interaction.reply({
+        content: "I'm not currently in a voice channel!",
+        ephemeral: true,
+      });
     }
-}
+
+    connection.destroy();
+    await interaction.reply("***Successfully left the voice channel***");
+  },
+};
