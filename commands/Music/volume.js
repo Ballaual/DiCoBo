4ea5a79171c505b6cf90fe406f7ed17f21be9ce3
Alexander Ballauf <ballaual@gmail.com>
2023-04-03 14:43:54 +0200

    fix permissions for volume command

--- commands/Music/volume.js
+++ commands/Music/volume.js
@@ -6,7 +6,7 @@ const distube = require("../../distubeClient")
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("volume")
-        .setDescription("Change the music player's volume.")
+        .setDescription("Changes the volume of the music player")
         .addIntegerOption(option =>
             option
                 .setName("amount")
@@ -15,41 +15,58 @@ module.exports = {
         ),
     timeout: 5000,
 
-    async execute (interaction) {
-        const volume = interaction.options.getInteger("amount")
-        const queue = await distube.getQueue(interaction)
-        const voiceChannel = interaction.member.voice.channel
-        // FIX needed
-        // const connection = client.voice.connections.get(interaction.guild.id);
-
-        if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
+    async execute(interaction) {
+        const volume = interaction.options.getInteger("amount");
+        const queue = await distube.getQueue(interaction);
+        const voiceChannelId = interaction.member.voice.channelId;
+      
+        if (!voiceChannelId) {
+          return interaction.reply({
+            content: "Please join a voice channel first!",
+            ephemeral: true,
+          });
         }
-
+      
+        const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
+      
+        if (!botMember.voice?.channelId) {
+          return interaction.reply({
+            content: "I'm not currently in a voice channel!",
+            ephemeral: true,
+          });
+        }
+      
+        const botVoiceChannelId = botMember.voice.channelId;
+      
+        if (voiceChannelId !== botVoiceChannelId) {
+          return interaction.reply({
+            content: "You need to be in the same voice channel as the bot to use this command!",
+            ephemeral: true,
+          });
+        }
+      
         if (!queue) {
-            const queueError = new EmbedBuilder()
-                .setDescription("There is Nothing Playing")
-                .setColor("#FF0000")
-
-            return interaction.reply({ embeds: [queueError] })
+          const queueError = new EmbedBuilder()
+            .setDescription("There is currently nothing playing!")
+            .setColor("#FF0000");
+      
+          return interaction.reply({ embeds: [queueError], ephemeral: true });
         }
-
-        // FIX needed
-        /* if (!connection || connection.channel.id !== voiceChannel.id) {
-      return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true });
-    } */
-
+      
         if (volume < 1 || volume > 200) {
-            return interaction.reply({ content: "Please enter a valid number (between 1 and 200)", ephemeral: true })
+          return interaction.reply({
+            content: "Please enter a valid number (between 1 and 200)",
+            ephemeral: true,
+          });
         }
-
-        await distube.setVolume(interaction, volume)
-
-        const total = 200
-        const current = volume
-        const bar = progressbar.splitBar(total, current, 27, "▬", "🔘")[0]
-
-        await interaction.reply(`Set the new volume to ${volume}%.`)
-        await interaction.channel.send(bar)
-    }
+      
+        await distube.setVolume(interaction, volume);
+      
+        const total = 200;
+        const current = volume;
+        const bar = progressbar.splitBar(total, current, 27, "▬", "🔘")[0];
+      
+        await interaction.reply(`Set the new volume to ${volume}%.`);
+        await interaction.channel.send(bar);
+      }                
 }
