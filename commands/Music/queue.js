2c2ff0088e080bd9bed932edad5b45d97d2ec2b7
Alexander Ballauf <ballaual@gmail.com>
2023-04-03 14:44:43 +0200

    set permissions for queue command

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -8,24 +8,14 @@ module.exports = {
         .setDescription("Displays the current music queue"),
     async execute (interaction) {
         const queue = await distube.getQueue(interaction)
-        const voiceChannel = interaction.member.voice?.channel
-
-        if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
-        }
 
         if (!queue) {
             const queueError = new EmbedBuilder()
-                .setDescription("There is nothing playing")
+                .setDescription("There is currently no song in the queue!")
                 .setColor("#FF0000")
 
             return interaction.reply({ embeds: [queueError] })
         }
-
-        // FIX needed
-        /* if (interaction.member.guild.me.voiceChannel.channelId !== voiceChannel.id) {
-      return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true });
-    } */
 
         const q = queue.songs.map((song, i) => {
             return `${i === 0 ? "Playing:" : `${i}.`} ${song.name} - \`${song.formattedDuration}\``
