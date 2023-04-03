2e5e8445c6649115bca8c6c1a555b2ddef2f8075
Alexander Ballauf <ballaual@gmail.com>
2023-04-03 17:23:05 +0200

    fix typo

--- handlers/guildDelete.js
+++ handlers/guildDelete.js
@@ -3,9 +3,9 @@ const { EmbedBuilder } = require("discord.js")
 const guildDelete = (client) => {
     client.on("guildDelete", guild => {
         const embed = new EmbedBuilder()
-            .setTitle("I left a server")
+            .setTitle("I've left a server")
             .setThumbnail(client.user.displayAvatarURL())
-            .setDescription(`I left ${guild.name} | ID ${guild.id}\n Server member: ${guild.memberCount}\nTotal server: ${client.guilds.cache.size}`)
+            .setDescription(`I've left ${guild.name} | ID ${guild.id}\n Server member: ${guild.memberCount}\nTotal server: ${client.guilds.cache.size}`)
             .setTimestamp()
         const logchannel = client.channels.cache.get(process.env.logChannel)
         logchannel.send({ embeds: [embed] })
