00ff288820f3dab3142812c7662bb7fdc7cb9806
Alexander Ballauf <ballaual@gmail.com>
2023-04-03 11:40:12 +0200

    eslint fixes

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,47 +1,46 @@
 const ready = (client) => {
     const updateActivity = () => {
-      if (client && client.guilds.cache.size > 0) {
-        const activities = [
-          { name: "/help" },
-          { name: `on ${client.guilds.cache.size} servers` },
-          { name: `with ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users` },
-          // add more activities as needed
-        ];
-  
-        console.log("\x1b[34m%s\x1b[0m", "Logging into Bot User...");
-        console.log("\x1b[34m%s\x1b[0m", `Logged in as ${client.user.tag} on ${client.guilds.cache.size} servers!`);
-  
-        let currentActivityIndex = 0;
-  
-        client.user.setPresence({
-          activities: [activities[currentActivityIndex]],
-          type: "LISTENING",
-          status: "online",
-        });
-  
-        setInterval(() => {
-          currentActivityIndex = (currentActivityIndex + 1) % activities.length;
-          client.user.setPresence({
-            activities: [activities[currentActivityIndex]],
-            type: "LISTENING",
-            status: "online",
-          });
-        }, 15000);
-      }
-    };
-  
+        if (client && client.guilds.cache.size > 0) {
+            const activities = [
+                { name: "/help" },
+                { name: `on ${client.guilds.cache.size} servers` },
+                { name: `with ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users` }
+                // add more activities as needed
+            ]
+
+            console.log("\x1b[34m%s\x1b[0m", "Logging into Bot User...")
+            console.log("\x1b[34m%s\x1b[0m", `Logged in as ${client.user.tag} on ${client.guilds.cache.size} servers!`)
+
+            let currentActivityIndex = 0
+
+            client.user.setPresence({
+                activities: [activities[currentActivityIndex]],
+                type: "LISTENING",
+                status: "online"
+            })
+
+            setInterval(() => {
+                currentActivityIndex = (currentActivityIndex + 1) % activities.length
+                client.user.setPresence({
+                    activities: [activities[currentActivityIndex]],
+                    type: "LISTENING",
+                    status: "online"
+                })
+            }, 15000)
+        }
+    }
+
     client.on("ready", () => {
-      updateActivity();
-    });
-  
+        updateActivity()
+    })
+
     client.on("guildMemberAdd", () => {
-      updateActivity();
-    });
-  
+        updateActivity()
+    })
+
     client.on("guildMemberRemove", () => {
-      updateActivity();
-    });
-  };
-  
-  module.exports = ready;
-  
+        updateActivity()
+    })
+}
+
+module.exports = ready
