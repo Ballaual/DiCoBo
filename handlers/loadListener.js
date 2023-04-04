6c07ed97f5984407a0b0f26b06098b0c247cde32
Alexander Ballauf <ballaual@gmail.com>
2023-04-04 23:04:03 +0200

    eslint --fix

--- handlers/loadListener.js
+++ handlers/loadListener.js
@@ -1,85 +1,84 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
-function loadListener(client) {
-  client.guilds.cache.forEach(guild => {
-    const guildID = guild.id;
-    const dataFilePath = path.join(__dirname, "..", "configs", "voice", `${guildID}.json`);
-    let data;
+function loadListener (client) {
+    client.guilds.cache.forEach(guild => {
+        const guildID = guild.id
+        const dataFilePath = path.join(__dirname, "..", "configs", "voice", `${guildID}.json`)
+        let data
 
-    try {
-      data = JSON.parse(fs.readFileSync(dataFilePath));
-    } catch (err) {
-      // Create the configs folder and the guild-specific JSON file if they don"t exist
-      fs.mkdirSync(path.join(__dirname, "..", "configs", "voice"), { recursive: true });
-      fs.writeFileSync(dataFilePath, JSON.stringify({}));
-      data = {};
-    }
-
-    // Set up the listener for each trigger channel
-    for (const [triggerChannelID, triggerChannelData] of Object.entries(data)) {
-      const triggerChannel = guild.channels.cache.get(triggerChannelID);
-
-      if (!triggerChannel) {
-        console.error(`Trigger channel not found: ${triggerChannelID}`);
-        continue;
-      }
-
-      const { name, category } = triggerChannelData;
-      const channelName = name;
-
-      const listener = async (oldState, newState) => {
-        // Check if a user joined the trigger channel
-        if (!oldState.channelId && newState.channelId === triggerChannel.id) {
-
-          // Create a new temporary voice channel in the specified category
-          const newChannel = await triggerChannel.guild.channels.create({
-            name: channelName,
-            type: 2,
-            parent: category,
-            // your permission overwrites or other options here
-          });
-
-          // Move the user to the new temporary voice channel
-          await newState.member.voice.setChannel(newChannel);
-
-          // Add the new temporary channel to the data object
-          data[triggerChannel.id] = {
-            ...data[triggerChannel.id],
-            createdChannels: {
-              ...data[triggerChannel.id].createdChannels,
-              [newChannel.id]: {
-                name: channelName,
-                category: category.id,
-                owner: newState.member.id
-              }
-            }
-          };
-          fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-
-          // Add the onLeave listener to the client
-          const onLeave = async (oldState, newState) => {
-            if (oldState.channelId === newChannel.id && newChannel.members.size === 0) {
-              // Delete the temporary channel when it becomes empty
-              await newChannel.delete();
-
-              // Remove the temporary channel data from the data object
-              delete data[triggerChannel.id].createdChannels[newChannel.id];
-              fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-
-              // Remove the onLeave listener from the client
-              client.off("voiceStateUpdate", onLeave);
-            }
-          };
-
-          // Add the onLeave listener to the client
-          client.on("voiceStateUpdate", onLeave);
+        try {
+            data = JSON.parse(fs.readFileSync(dataFilePath))
+        } catch (err) {
+            // Create the configs folder and the guild-specific JSON file if they don"t exist
+            fs.mkdirSync(path.join(__dirname, "..", "configs", "voice"), { recursive: true })
+            fs.writeFileSync(dataFilePath, JSON.stringify({}))
+            data = {}
         }
-      };
 
-      client.on("voiceStateUpdate", listener);
-    }
-  });
+        // Set up the listener for each trigger channel
+        for (const [triggerChannelID, triggerChannelData] of Object.entries(data)) {
+            const triggerChannel = guild.channels.cache.get(triggerChannelID)
+
+            if (!triggerChannel) {
+                console.error(`Trigger channel not found: ${triggerChannelID}`)
+                continue
+            }
+
+            const { name, category } = triggerChannelData
+            const channelName = name
+
+            const listener = async (oldState, newState) => {
+                // Check if a user joined the trigger channel
+                if (!oldState.channelId && newState.channelId === triggerChannel.id) {
+                    // Create a new temporary voice channel in the specified category
+                    const newChannel = await triggerChannel.guild.channels.create({
+                        name: channelName,
+                        type: 2,
+                        parent: category
+                        // your permission overwrites or other options here
+                    })
+
+                    // Move the user to the new temporary voice channel
+                    await newState.member.voice.setChannel(newChannel)
+
+                    // Add the new temporary channel to the data object
+                    data[triggerChannel.id] = {
+                        ...data[triggerChannel.id],
+                        createdChannels: {
+                            ...data[triggerChannel.id].createdChannels,
+                            [newChannel.id]: {
+                                name: channelName,
+                                category: category.id,
+                                owner: newState.member.id
+                            }
+                        }
+                    }
+                    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2))
+
+                    // Add the onLeave listener to the client
+                    const onLeave = async (oldState, newState) => {
+                        if (oldState.channelId === newChannel.id && newChannel.members.size === 0) {
+                            // Delete the temporary channel when it becomes empty
+                            await newChannel.delete()
+
+                            // Remove the temporary channel data from the data object
+                            delete data[triggerChannel.id].createdChannels[newChannel.id]
+                            fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2))
+
+                            // Remove the onLeave listener from the client
+                            client.off("voiceStateUpdate", onLeave)
+                        }
+                    }
+
+                    // Add the onLeave listener to the client
+                    client.on("voiceStateUpdate", onLeave)
+                }
+            }
+
+            client.on("voiceStateUpdate", listener)
+        }
+    })
 }
 
-module.exports = loadListener;
+module.exports = loadListener
--- handlers/ready.js
+++ handlers/ready.js
@@ -3,54 +3,53 @@ const loadListener = require("./loadListener")
 const ready = (client) => {
     let serverCount = 0
     let memberCount = 0
-  
+
     const updateActivity = () => {
-      if (client && client.guilds.cache.size > 0) {
-        serverCount = client.guilds.cache.size
-        memberCount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
-  
-        const activities = [
-          { name: "/help" },
-          { name: `on ${serverCount} servers` },
-          { name: `with ${memberCount} users` }
-          // add more activities as needed
-        ]
-  
-        console.log("\x1b[34m%s\x1b[0m", "Logging into Bot User...")
-        console.log("\x1b[34m%s\x1b[0m", `Logged in as ${client.user.tag} on ${serverCount} servers!`)
-  
-        let currentActivityIndex = 0
-  
-        client.user.setPresence({
-          activities: [activities[currentActivityIndex]],
-          type: "PLAYING",
-          status: "online"
-        })
-  
-        setInterval(() => {
-          currentActivityIndex = (currentActivityIndex + 1) % activities.length
-          client.user.setPresence({
-            activities: [activities[currentActivityIndex]],
-            type: "PLAYING",
-            status: "online"
-          })
-        }, 15000)
-      }
+        if (client && client.guilds.cache.size > 0) {
+            serverCount = client.guilds.cache.size
+            memberCount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
+
+            const activities = [
+                { name: "/help" },
+                { name: `on ${serverCount} servers` },
+                { name: `with ${memberCount} users` }
+                // add more activities as needed
+            ]
+
+            console.log("\x1b[34m%s\x1b[0m", "Logging into Bot User...")
+            console.log("\x1b[34m%s\x1b[0m", `Logged in as ${client.user.tag} on ${serverCount} servers!`)
+
+            let currentActivityIndex = 0
+
+            client.user.setPresence({
+                activities: [activities[currentActivityIndex]],
+                type: "PLAYING",
+                status: "online"
+            })
+
+            setInterval(() => {
+                currentActivityIndex = (currentActivityIndex + 1) % activities.length
+                client.user.setPresence({
+                    activities: [activities[currentActivityIndex]],
+                    type: "PLAYING",
+                    status: "online"
+                })
+            }, 15000)
+        }
     }
-  
+
     client.on("ready", () => {
-      updateActivity()
-      //loadListener(client)
+        updateActivity()
+        // loadListener(client)
     })
-  
+
     client.on("guildMemberAdd", () => {
-      updateActivity()
+        updateActivity()
     })
-  
+
     client.on("guildMemberRemove", () => {
-      updateActivity()
+        updateActivity()
     })
-  }
-  
-  module.exports = ready
-  
+}
+
+module.exports = ready
