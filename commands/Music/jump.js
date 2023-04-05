b9ac151bc499d68cf553961e274d0c47d608e738
Alexander Ballauf <ballaual@gmail.com>
2023-04-05 22:54:46 +0200

    implement jump command

---
+++ commands/Music/jump.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+const distubeClient = require("../../distubeClient");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("jump")
+        .setDescription("Jump to the song number in the queue")
+        .addIntegerOption(option => 
+            option.setName("id")
+                .setDescription("The music's ID in the queue")
+                .setRequired(true)),
+    async execute(interaction) {
+        const musicId = interaction.options.getInteger("id");
+        const queue = await distubeClient.getQueue(interaction);
+        const voiceChannelId = interaction.member.voice.channelId;
+
+        if (!voiceChannelId) {
+            return interaction.reply({
+                content: "Please join a voice channel first!",
+                ephemeral: true
+            })
+        }
+
+        if (!queue) {
+            const queueError = new EmbedBuilder()
+                .setDescription("There is Nothing There is currently nothing to skip!")
+                .setColor("FF0000");
+            return interaction.reply({ embeds: [queueError] });
+        }
+
+        const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
+
+        if (!botMember.voice?.channelId) {
+            return interaction.reply({
+                content: "I am not currently in a voice channel!",
+                ephemeral: true
+            })
+        }
+
+        const botVoiceChannelId = botMember.voice.channelId;
+
+        if (voiceChannelId !== botVoiceChannelId) {
+            return interaction.reply({
+                content: "You need to be in the same voice channel as the bot to use this command!",
+                ephemeral: true
+            })
+        }
+
+        try {
+            await distubeClient.jump(interaction, parseInt(musicId));
+            await interaction.reply(`⏩ | ***Jumped to song number ${musicId} in the queue***`)
+        } catch (error) {
+            return interaction.reply({content: "Invalid song ID!)", ephemeral: true});
+        }
+    },
+};
