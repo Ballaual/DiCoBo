55159299dbed605d0cd27ffaafa713e6b19a5ff3
Alexander Ballauf <ballaual@gmail.com>
2023-04-05 22:26:14 +0200

    implement autoplay command

---
+++ commands/Music/autoplay.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
+const distube = require("../../distubeClient");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("autoplay")
+    .setDescription("Enable or disable autoplay"),
+
+  async execute(interaction) {
+    const queue = await distube.getQueue(interaction);
+    const voiceChannelId = interaction.member.voice.channelId;
+
+    if (!voiceChannelId) {
+      return interaction.reply({
+        content: "Please join a voice channel first!",
+        ephemeral: true,
+      });
+    }
+
+    const botMember = interaction.guild.members.cache.get(
+      interaction.client.user.id
+    );
+
+    if (!botMember.voice?.channelId) {
+      return interaction.reply({
+        content: "I am not currently in a voice channel!",
+        ephemeral: true,
+      });
+    }
+
+    const botVoiceChannelId = botMember.voice.channelId;
+
+    if (voiceChannelId !== botVoiceChannelId) {
+      return interaction.reply({
+        content:
+          "You need to be in the same voice channel as the bot to use this command!",
+        ephemeral: true,
+      });
+    }
+
+    if (!queue) {
+      const queueError = new EmbedBuilder()
+        .setDescription("There is currently nothing to autoplay!")
+        .setColor("#FF0000");
+
+      return interaction.reply({ embeds: [queueError], ephemeral: true });
+    }
+
+    const mode = distube.toggleAutoplay(interaction);
+    return interaction.reply(
+      `Set autoplay mode to \`${mode ? "On" : "Off"}\``
+    );
+  },
+};
