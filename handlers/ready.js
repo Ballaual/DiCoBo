e64ffe26593e72fe9d11f4d8f19d9f76417c3c8d
Alexander Ballauf <ballaual@gmail.com>
2023-05-08 10:31:28 +0200

    removed buggy presence status updates

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,17 +1,12 @@
 const ready = (client) => {
-    let serverCount = 0
-    let memberCount = 0
+
+let serverCount = 0
 
     const updateActivity = () => {
         if (client && client.guilds.cache.size > 0) {
-            serverCount = client.guilds.cache.size
-            memberCount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
 
             const activities = [
                 { name: "/help" },
-                { name: `on ${serverCount} servers` },
-                { name: `with ${memberCount} users` }
-                // add more activities as needed
             ]
 
             console.log("\x1b[34m%s\x1b[0m", "Logging into Bot User...")
@@ -21,30 +16,14 @@ const ready = (client) => {
 
             client.user.setPresence({
                 activities: [activities[currentActivityIndex]],
-                type: "PLAYING",
+                type: "ONLINE",
                 status: "online"
             })
 
-            setInterval(() => {
-                currentActivityIndex = (currentActivityIndex + 1) % activities.length
-                client.user.setPresence({
-                    activities: [activities[currentActivityIndex]],
-                    type: "PLAYING",
-                    status: "online"
-                })
-            }, 15000)
         }
     }
 
     client.on("ready", () => {
-        updateActivity()
-    })
-
-    client.on("guildMemberAdd", () => {
-        updateActivity()
-    })
-
-    client.on("guildMemberRemove", () => {
         updateActivity()
     })
 }
