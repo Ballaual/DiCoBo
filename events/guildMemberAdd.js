3d9bd571850bc0338b34db31065f7d53c70a907e
Alexander Ballauf <ballaual@gmail.com>
2023-05-19 17:52:50 +0200

    Feat: welcome msg to user when joining a discord

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'guildMemberAdd',
+  once: false,
+  async execute(member) {
+    try {
+      const { user, guild } = member;
+      const serverName = guild.name;
+      const dmChannel = await user.createDM();
+      await dmChannel.send(`Hello ${user.username}, welcome to \`${serverName}\`!`);
+    } catch (error) {
+      console.error('There was an error sending the dm:', error);
+    }
+  },
+};
