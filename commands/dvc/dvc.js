9b23c1af89d5851c79b662d5a37c5c7aff72c5ae
Alexander Ballauf <ballaual@gmail.com>
2023-06-02 16:46:00 +0200

    Fix: config files for dvc are now beeing written correctly

--- commands/dvc/dvc.js
+++ commands/dvc/dvc.js
@@ -2,96 +2,104 @@ const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { PermissionFlagsBits } = require('discord.js');
 
+const getGuildFilePath = (guildId) => `./config/dvc/${guildId}.json`;
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('dvc')
-		.setDescription('Defines a voice channel as voice creator')
-		.setDMPermission(false)
-		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('add')
-				.setDescription('Sets a voice channel as voice cretor')
-				.addChannelOption(option =>
-					option
-						.setName('channel')
-						.setDescription('Select')
-						.setRequired(true),
-				)
-				.addChannelOption(option =>
-					option
-						.setName('category')
-						.setDescription('The channel category where the user channel gets created')
-						.setRequired(true),
-				),
-		)
-		.addSubcommand(subcommand =>
-			subcommand
-				.setName('remove')
-				.setDescription('Removes a voice channel as voice creator and the channel itself')
-				.addChannelOption(option =>
-					option
-						.setName('channel')
-						.setDescription('The channel to be removed')
-						.setRequired(true),
-				),
-		),
-	category: 'dvc',
-	async execute(interaction) {
-		const subcommand = interaction.options.getSubcommand();
+  data: new SlashCommandBuilder()
+    .setName('dvc')
+    .setDescription('Defines a voice channel as voice creator')
+    .setDMPermission(false)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ADMINISTRATOR)
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('add')
+        .setDescription('Sets a voice channel as voice creator')
+        .addChannelOption(option =>
+          option
+            .setName('channel')
+            .setDescription('Select')
+            .setRequired(true),
+        )
+        .addChannelOption(option =>
+          option
+            .setName('category')
+            .setDescription('The channel category where the user channel gets created')
+            .setRequired(true),
+        ),
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('remove')
+        .setDescription('Removes a voice channel as voice creator and the channel itself')
+        .addChannelOption(option =>
+          option
+            .setName('channel')
+            .setDescription('The channel to be removed')
+            .setRequired(true),
+        ),
+    ),
+  category: 'dvc',
+  async execute(interaction) {
+    const subcommand = interaction.options.getSubcommand();
+    const guildId = interaction.guildId;
+    const filePath = getGuildFilePath(guildId);
 
-		try {
-			if (subcommand === 'add') {
-				const channelId = interaction.options.getChannel('channel').id;
-				const categoryId = interaction.options.getChannel('category').id;
+    try {
+      if (subcommand === 'add') {
+        const channelId = interaction.options.getChannel('channel').id;
+        const categoryId = interaction.options.getChannel('category').id;
 
-				const data = JSON.parse(fs.readFileSync('./config/dvc.json', 'utf8'));
+        let data = {};
+        if (fs.existsSync(filePath)) {
+          data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        }
 
-				if (data.creatorChannels && data.creatorChannels.includes(channelId)) {
-					return await interaction.reply({ content: 'The channel is already a voice creator.', ephemeral:true });
-				}
+        if (data.creatorChannels && data.creatorChannels.includes(channelId)) {
+          return await interaction.reply({ content: 'The channel is already a voice creator.', ephemeral: true });
+        }
 
-				if (!data.creatorChannels) {
-					data.creatorChannels = [];
-				}
-				data.creatorChannels.push(channelId);
+        if (!data.creatorChannels) {
+          data.creatorChannels = [];
+        }
+        data.creatorChannels.push(channelId);
 
-				data.categories = data.categories || {};
-				data.categories[channelId] = categoryId;
+        data.categories = data.categories || {};
+        data.categories[channelId] = categoryId;
 
-				fs.writeFileSync('./config/dvc.json', JSON.stringify(data));
+        fs.writeFileSync(filePath, JSON.stringify(data));
 
-				await interaction.reply({ content: `Successfully set channel id: ${channelId} in category id: ${categoryId} as a voice creator.`, ephemeral: true });
-			}
-			else if (subcommand === 'remove') {
-				const channelId = interaction.options.getChannel('channel').id;
+        await interaction.reply({ content: `Successfully set channel id: ${channelId} in category id: ${categoryId} as a voice creator.`, ephemeral: true });
+      } else if (subcommand === 'remove') {
+        const channelId = interaction.options.getChannel('channel').id;
 
-				const data = JSON.parse(fs.readFileSync('./config/dvc.json', 'utf8'));
+        let data = {};
+        if (fs.existsSync(filePath)) {
+          data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        }
 
-				if (!data.creatorChannels || !data.creatorChannels.includes(channelId)) {
-					return await interaction.reply({ content:'The channel is no voice creator.', ephemeral: true });
-				}
+        if (!data.creatorChannels || !data.creatorChannels.includes(channelId)) {
+          return await interaction.reply({ content: 'The channel is not a voice creator.', ephemeral: true });
+        }
 
-				data.creatorChannels = data.creatorChannels.filter(id => id !== channelId);
+        data.creatorChannels = data.creatorChannels.filter(id => id !== channelId);
 
-				if (data.categories && data.categories[channelId]) {
-					delete data.categories[channelId];
-				}
+        if (data.categories && data.categories[channelId]) {
+          delete data.categories[channelId];
+        }
 
-				fs.writeFileSync('./config/dvc.json', JSON.stringify(data));
+        fs.writeFileSync(filePath, JSON.stringify(data));
 
-				const guild = interaction.guild;
-				const channel = guild.channels.cache.get(channelId);
-				if (channel) {
-					channel.delete();
-				}
+        const guild = interaction.guild;
+        const channel = guild.channels.cache.get(channelId);
+        if (channel) {
+          channel.delete();
+        }
 
-				await interaction.reply({ content:'Voice creator successfully removed.', ephemeral: true });
-			}
-		}
-		catch (error) {
-			console.error(error);
-			await interaction.reply({ content:'An internal error occured.', ephemeral: true });
-		}
-	},
+        await interaction.reply({ content: 'Voice creator successfully removed.', ephemeral: true });
+      }
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'An internal error occurred.', ephemeral: true });
+    }
+  },
 };
--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,98 +2,95 @@ const { Events, ChannelType, PermissionsBitField } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
-const directory = './config';
-const filePath = path.join(directory, 'dvc.json');
+const directory = './config/dvc';
+const getGuildFilePath = (guildId) => path.join(directory, `${guildId}.json`);
 
 if (!fs.existsSync(directory)) {
-	fs.mkdirSync(directory);
-}
-
-if (!fs.existsSync(filePath)) {
-	const initialData = {
-		creatorChannels: [],
-		categories: {},
-		userChannels: {},
-	};
-	fs.writeFileSync(filePath, JSON.stringify(initialData));
+  fs.mkdirSync(directory, { recursive: true });
 }
 
 module.exports = {
-	name: Events.VoiceStateUpdate,
-	async execute(oldState, newState) {
-		const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-		const creatorChannels = data.creatorChannels;
-		const categories = data.categories;
-		const userChannels = data.userChannels;
+  name: Events.VoiceStateUpdate,
+  async execute(oldState, newState) {
+    const guildId = newState.guild.id;
+    const filePath = getGuildFilePath(guildId);
 
-		if (newState.channel && creatorChannels.includes(newState.channel.id) && newState.channel.members.size === 1) {
-			const guild = newState.guild;
-			const user = newState.member.user;
+    let data = {};
+    if (fs.existsSync(filePath)) {
+      data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } else {
+      const initialData = {
+        creatorChannels: [],
+        categories: {},
+        userChannels: {},
+      };
+      fs.writeFileSync(filePath, JSON.stringify(initialData));
+    }
 
-			let existingChannel = guild.channels.cache.find(channel => channel.name === `${user.username}'s Channel`);
+    const creatorChannels = data.creatorChannels;
+    const categories = data.categories;
+    const userChannels = data.userChannels || {};
 
-			if (!existingChannel) {
-				const categoryId = categories[newState.channel.id];
-				const category = guild.channels.cache.get(categoryId);
+    if (newState.channel && creatorChannels.includes(newState.channel.id) && newState.channel.members.size === 1) {
+      const guild = newState.guild;
+      const user = newState.member.user;
 
-				if (category && category.type === ChannelType.GuildCategory) {
-					const newChannel = await guild.channels.create({
-						name: `${user.username}'s Channel`,
-						type: ChannelType.GuildVoice,
-						parent: category,
-						userLimit: newState.channel.userLimit,
-						bitrate: newState.channel.bitrate,
-						permissionOverwrites: [
-							{
-								id: user.id,
-								allow: [PermissionsBitField.Flags.ManageChannels, PermissionsBitField.Flags.ManageRoles],
-							},
-						],
-					});
+      let existingChannel = guild.channels.cache.find(channel => channel.name === `${user.username}'s Channel`);
 
-					newState.setChannel(newChannel);
+      if (!existingChannel) {
+        const categoryId = categories[newState.channel.id];
+        const category = guild.channels.cache.get(categoryId);
 
-					const creatorChannel = guild.channels.cache.get(newState.channel.id);
-					if (creatorChannel) {
-						userChannels[newChannel.id] = {
-							creatorChannelId: creatorChannel.id,
-						};
+        if (category && category.type === ChannelType.GuildCategory) {
+          const newChannel = await guild.channels.create({
+            name: `${user.username}'s Channel`,
+            type: ChannelType.GuildVoice,
+            parent: category,
+            userLimit: newState.channel.userLimit,
+            bitrate: newState.channel.bitrate,
+            permissionOverwrites: [
+              {
+                id: user.id,
+                allow: [PermissionsBitField.Flags.ManageChannels, PermissionsBitField.Flags.ManageRoles],
+              },
+            ],
+          });
 
-						existingChannel = newChannel;
-					}
-				}
-			}
-		}
+          newState.setChannel(newChannel);
 
-		if (
-			oldState.channel &&
-			oldState.channel.name.includes(`${oldState.member.user.username}'s Channel`) &&
-			oldState.member.user.id === newState.member.user.id
-		) {
-			if(oldState.channel.members.size > 0) {
-				// Get the next member in the channel
-				const nextMember = oldState.channel.members.first();
+          const creatorChannel = guild.channels.cache.get(newState.channel.id);
+          if (creatorChannel) {
+            userChannels[newChannel.id] = {
+              creatorChannelId: creatorChannel.id,
+            };
 
-				// Change channel owner
-				await oldState.channel.permissionOverwrites.edit(nextMember, { ManageChannels: true, ManageRoles: true });
-				await oldState.channel.permissionOverwrites.delete(oldState.member);
+            existingChannel = newChannel;
+          }
+        }
+      }
+    }
 
-				// Rename channel
-				await oldState.channel.edit({ name: `${nextMember.user.username}'s Channel` });
-			} else {
-				// Delete channel if there are no members left
-				oldState.channel.delete();
-			}
+    if (
+      oldState.channel &&
+      oldState.channel.name.includes(`${oldState.member.user.username}'s Channel`) &&
+      oldState.member.user.id === newState.member.user.id
+    ) {
+      if (oldState.channel.members.size > 0) {
+        const nextMember = oldState.channel.members.first();
+        await oldState.channel.permissionOverwrites.edit(nextMember, { ManageChannels: true, ManageRoles: true });
+        await oldState.channel.permissionOverwrites.delete(oldState.member);
+        await oldState.channel.edit({ name: `${nextMember.user.username}'s Channel` });
+      } else {
+        if (userChannels[oldState.channel.id]) {
+          const creatorChannel = newState.guild.channels.cache.get(userChannels[oldState.channel.id].creatorChannelId);
+          if (creatorChannel) {
+            delete userChannels[oldState.channel.id];
+          }
+        }
+        oldState.channel.delete();
+      }
+    }
 
-			// Update data
-			if (userChannels[oldState.channel.id]) {
-				const creatorChannel = newState.guild.channels.cache.get(userChannels[oldState.channel.id].creatorChannelId);
-				if (creatorChannel) {
-					delete userChannels[oldState.channel.id];
-				}
-			}
-		}
-
-		fs.writeFileSync(filePath, JSON.stringify(data));
-	},
+    fs.writeFileSync(filePath, JSON.stringify({ ...data, userChannels }));
+  },
 };
