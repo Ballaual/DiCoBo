42ac6777e6f48404f04d693c5de9fc38e3154360
Alexander Ballauf <ballaual@gmail.com>
2023-07-18 16:08:11 +0200

    Feat: write new userlimit into the config

--- commands/dvc/limit.js
+++ commands/dvc/limit.js
@@ -1,42 +1,60 @@
 const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const directory = './config/dvc';
+const getGuildFilePath = (guildId) => path.join(directory, `${guildId}.json`);
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('limit')
-		.setDescription('Sets the user limit for the voice channel.')
-		.setDMPermission(false)
-		.addIntegerOption(option =>
-			option.setName('limit')
-				.setDescription('The user limit for the voice channel.')
-				.setRequired(true)),
+    data: new SlashCommandBuilder()
+        .setName('limit')
+        .setDescription('Sets the user limit for the voice channel.')
+        .setDefaultPermission(false)
+        .addIntegerOption(option =>
+            option.setName('limit')
+                .setDescription('The user limit for the voice channel.')
+                .setRequired(true)),
 
-	async execute(interaction) {
-		const channel = interaction.member.voice.channel;
+    async execute(interaction) {
+        const channel = interaction.member.voice.channel;
 
-		if (!channel) {
-			return interaction.reply('You must be in a voice channel to use this command.');
-		}
+        if (!channel) {
+            return interaction.reply('You must be in a voice channel to use this command.');
+        }
 
-		if (!channel.permissionsFor(interaction.user).has(PermissionsBitField.Flags.ManageChannels)) {
-			return interaction.reply('You do not have permissions to manage this channel.');
-		}
+        if (!channel.permissionsFor(interaction.user).has(PermissionsBitField.Flags.ManageChannels)) {
+            return interaction.reply('You do not have permissions to manage this channel.');
+        }
 
-		const userLimit = interaction.options.getInteger('limit');
+        const userLimit = interaction.options.getInteger('limit');
 
-		if (userLimit === null || userLimit < 0) {
-			return interaction.reply('Invalid user limit specified.');
-		}
+        if (userLimit === null || userLimit < 0) {
+            return interaction.reply('Invalid user limit specified.');
+        }
 
-		try {
-			await channel.setUserLimit(userLimit);
+        try {
+            await channel.setUserLimit(userLimit);
 
-			const limit = userLimit === 0 ? 'unlimited' : userLimit.toString();
+            const limit = userLimit === 0 ? 'unlimited' : userLimit.toString();
 
-			return interaction.reply(`The user limit for the voice channel has been set to \`${limit}\`.`);
-		}
-		catch (error) {
-			console.error('Failed to set user limit for the channel:', error);
-			return interaction.reply('An error occurred while setting the user limit for the channel.');
-		}
-	},
+            const guildId = interaction.guild.id;
+            const filePath = getGuildFilePath(guildId);
+
+            if (fs.existsSync(filePath)) {
+                const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+                const userChannels = data.userChannels || {};
+
+                if (userChannels[channel.id]) {
+                    userChannels[channel.id].userLimit = userLimit;
+                }
+
+                fs.writeFileSync(filePath, JSON.stringify({ ...data, userChannels }));
+            }
+
+            return interaction.reply(`The user limit for the voice channel has been set to \`${limit}\`.`);
+        } catch (error) {
+            console.error('Failed to set user limit for the channel:', error);
+            return interaction.reply('An error occurred while setting the user limit for the channel.');
+        }
+    },
 };
